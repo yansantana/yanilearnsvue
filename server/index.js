fe389dcf144ac7b9b637f13b6f3336e3fe28a0a0
Marianne Santana <santanamarianne.v@gmail.com>
2020-06-28 17:48:12 +0800

    Fixed backend

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-const posts = require('./routes/api/posts.js')
+const posts = require('./routes/api/posts')
 
 app.use('/api/posts' , posts)
 
